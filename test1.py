da281d17859543e33e91a16cce38c0f7878bc92f
Christian Solje <csolje@gmail.com>
2018-04-10 18:17:04 +0800

    Adding a test python file

---
+++ test1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 17:04:41 2018
+
+@author: csolje
+"""
+
+FirstName = input("Enter your name: ")
+
+def printName(name):
+    print("Hello my name is", name)
+
+printName(FirstName)
