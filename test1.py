b8367d909689e01b3a8b29e0c47cd22edbbd7fc6
Christian Solje <csolje@gmail.com>
2018-04-12 13:54:41 +0800

    Editted the test1.py with fix variables

--- test1.py
+++ test1.py
@@ -6,9 +6,11 @@ Created on Tue Apr 10 17:04:41 2018
 @author: csolje
 """
 
-FirstName = input("Enter your name: ")
+FirstName = "Chris"  # input("Enter your name: ")
+
 
 def printName(name):
     print("Hello", name, "how are you")
+
 
 printName(FirstName)
